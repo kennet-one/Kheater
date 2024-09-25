51b2f794a4967cf77435adf92624f8313a4ed555
kennet-one <kennet1one@gmail.com>
2024-09-25 21:16:10 +0200

    update heat core

--- Kheater.ino
+++ Kheater.ino
@@ -15,9 +15,35 @@ enum HEAT {
   HE0,
   HE1,
   HE2,
-  HE3
-} HEAT = HE0;
+  HE3,
+  HE4
+} heat = HE4;
 
+void heatcore () {
+  switch (heat) {
+    case HE0:
+      digitalWrite(13, HIGH);  // вкл кулер
+
+      digitalWrite(27, HIGH);   //викл реле L
+      break;
+
+    case HE1:
+      digitalWrite(13, HIGH);  // вкл кулер
+      digitalWrite(27, LOW); //вкл реле L
+      break;
+
+    case HE2:
+      
+      break;
+    case HE3:
+      
+      break;
+    case HE4:
+      digitalWrite(13, LOW);
+      digitalWrite(27, HIGH); //викл реле L
+      break;
+  }
+}
 
 void receivedCallback( uint32_t from, String &msg ) {
 
@@ -26,21 +52,26 @@ void receivedCallback( uint32_t from, String &msg ) {
   String str3 = "he1";
   String str4 = "he2";
   String str5 = "he3";
+  String str6 = "he4";
 
   if (str1.equals(str2)) { // просто кулер
-
+    heat = HE0;
   }
 
   if (str1.equals(str3)) { // кулер + нагрев L
-
+    heat = HE1;
   }
   
   if (str1.equals(str4)) { // кулер + нагрев H
-
+    heat = HE2;
   }
   
   if (str1.equals(str5)) { // кулер + нагрев L + H
+    heat = HE3;
+  }
 
+  if (str1.equals(str6)) { // виключано
+    heat = HE4;
   }
 }
 
@@ -48,10 +79,14 @@ void receivedCallback( uint32_t from, String &msg ) {
 void setup() {
   Serial.begin(115200);
 
-  pinMode(13, INPUT); //реле кулера
-  pinMode(12, INPUT); //реле обороту корпуса
-  pinMode(14, INPUT); //реле H
-  pinMode(27, INPUT); //реле L
+  pinMode(13, OUTPUT); //реле кулера
+  pinMode(12, OUTPUT); //реле обороту корпуса
+  pinMode(14, OUTPUT); //реле H
+  pinMode(27, OUTPUT); //реле L
+
+  digitalWrite(27, HIGH);
+  digitalWrite(12, HIGH);
+  digitalWrite(14, HIGH);
 
   mesh.init( MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT );
   mesh.onReceive(&receivedCallback);
@@ -59,6 +94,8 @@ void setup() {
 }
 
 void loop() {
+
+  heatcore ();
 
   mesh.update();
 }
