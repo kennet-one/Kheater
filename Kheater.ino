c9f2b4ca547dfcc711760f11b3a9e818ccb17fb1
kennet-one <kennet1one@gmail.com>
2024-09-27 19:30:30 +0200

    add 50/50 load timer

--- Kheater.ino
+++ Kheater.ino
@@ -14,6 +14,10 @@ painlessMesh  mesh;
 unsigned long he4t = 0;
 bool he4timer = false;
 
+unsigned long he55 = 1800000;
+bool he55timer = true;
+bool he55f = false;
+
 enum HEAT {
   HE0,
   HE1,
@@ -44,7 +48,19 @@ void heatcore () {
     case HE4:
       digitalWrite(13, LOW);
       digitalWrite(27, HIGH); //викл реле L
+      he55f = false;
       break;
+  }
+}
+
+void loop55timer () {
+  if (he55f) {
+    if ((millis() - he55 >= 1800000) && (he55timer)) { // 30 минут
+      heat = HE1;
+    }
+    if ((millis() - he55 >= 1800000) && (!he55timer)) { // 30 минут
+      heat = HE0;
+    }
   }
 }
 
@@ -65,6 +81,7 @@ void receivedCallback( uint32_t from, String &msg ) {
   String str4 = "he2";
   String str5 = "he3";
   String str6 = "he4";
+  String str7 = "he55";
 
   if (str1.equals(str2)) { // просто кулер
     heat = HE0;
@@ -86,6 +103,10 @@ void receivedCallback( uint32_t from, String &msg ) {
     heat = HE0;
     he4t = millis();
     he4timer = true;
+  }
+
+  if (str1.equals(str7)) { // 50/50 таймер
+    he55f = !he55f;
   }
 }
 
@@ -111,6 +132,8 @@ void loop() {
 
   safetimer ();
   heatcore ();
+
+  loop55timer ();
 
   mesh.update();
 }
